41f40783b9eef8adc31a27d361e6842dca5343ff
“Raquel <raquelmichelonaulas@gmail.com>
2022-05-20 10:41:38 +0100

    refactored business rules

--- src/main/java/com/algaworks/algalog/domain/service/SolicitacaoEntregaService.java
+++ src/main/java/com/algaworks/algalog/domain/service/SolicitacaoEntregaService.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.algaworks.algalog.domain.model.Cliente;
 import com.algaworks.algalog.domain.model.Entrega;
 import com.algaworks.algalog.domain.model.StatusEntrega;
 import com.algaworks.algalog.domain.repository.EntregaRepository;
@@ -15,11 +16,16 @@ import lombok.AllArgsConstructor;
 @Service
 public class SolicitacaoEntregaService {
 	
+	private CatalogoClienteService catalogoClienteService;
 	private EntregaRepository entregaRepository;
 	
 	@Transactional
 	public Entrega solicitar(Entrega entrega) {
 		//inserir aqui as regras de negocio da solicitacao de uma entrega
+		
+		Cliente cliente = catalogoClienteService.buscar(entrega.getCliente().getId());
+		
+		entrega.setCliente(cliente);
 		
 		//por padrao entra com status pendente
 		entrega.setStatus(StatusEntrega.PENDENTE);
