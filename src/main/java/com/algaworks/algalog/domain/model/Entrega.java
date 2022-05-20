e65fc77ead51a8ddb3a216aa6a23e67e4bb93888
“Raquel <raquelmichelonaulas@gmail.com>
2022-05-20 08:34:56 +0100

    generated getters, setters and equals end hash codes with lombok and object relacional mapping with jakarta persistence

--- src/main/java/com/algaworks/algalog/domain/model/Entrega.java
+++ src/main/java/com/algaworks/algalog/domain/model/Entrega.java
@@ -3,16 +3,40 @@ package com.algaworks.algalog.domain.model;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded=true)
+@Entity //Jakarta Persistence
 public class Entrega {
 	
+	@EqualsAndHashCode.Include //gera equals and hash code apenas para id
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@ManyToOne //muitas entregas possuem um cliente / JoinColumn padrao eh cliente_id
 	private Cliente cliente;
 	
+	@Embedded //abstrair os dados do destinatario para outra classe mas mapeando para a tabela entrega
 	private Destinatario destinatario;
 	
 	private BigDecimal taxa;
 	
+	@Enumerated(EnumType.STRING) //para armazenar a string que representa a enumeracao
 	private StatusEntrega status;
 	
 	private LocalDateTime dataPedido;
